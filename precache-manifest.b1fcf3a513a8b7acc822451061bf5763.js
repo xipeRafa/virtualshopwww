d42e0ba6b7c710037eb28b66272b1618acfe0f6a
rafael <rafaelsistem87@gmail.com>
2022-01-25 20:07:54 -0700

    Updates

---
+++ precache-manifest.b1fcf3a513a8b7acc822451061bf5763.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e012a3e4f7dc74f24d20eab837a4e2",
+    "url": "/virtualshopwww/index.html"
+  },
+  {
+    "revision": "722471475ae1dc5d39cd",
+    "url": "/virtualshopwww/static/css/2.ef07fde4.chunk.css"
+  },
+  {
+    "revision": "fc37b28d3fd9e2dac877",
+    "url": "/virtualshopwww/static/css/main.60bf6b0e.chunk.css"
+  },
+  {
+    "revision": "722471475ae1dc5d39cd",
+    "url": "/virtualshopwww/static/js/2.0d9a4f89.chunk.js"
+  },
+  {
+    "revision": "e1a48f7972a3d6d14940cfe1336f1b84",
+    "url": "/virtualshopwww/static/js/2.0d9a4f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc37b28d3fd9e2dac877",
+    "url": "/virtualshopwww/static/js/main.40fd32dd.chunk.js"
+  },
+  {
+    "revision": "418b85f6e10940d7deaf",
+    "url": "/virtualshopwww/static/js/runtime-main.73d6b3e9.js"
+  }
+]);
